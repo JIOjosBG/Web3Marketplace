d6010e211070b356b28766cf59aeda521ba30a40
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-11-20 20:32:36 +0200

    feat: add pay and deliver product functions

--- hardhat/contracts/SimpleSeller.sol
+++ hardhat/contracts/SimpleSeller.sol
@@ -7,25 +7,31 @@ import "./Marketplace.sol";
 contract SimpleSeller is Ownable{
     struct Product{
         string name;
-        uint userPays;
+        uint price;
         uint sellerGets;
         address seller;
         uint addDate;
         string linkForMedia;
         bytes32 marketHashOfData;
-        bool approved;  
+        bool approved;
+        bool paid;
+        bool received; 
     }
     address public  ownerMarketplace;
-    Product[] products;
-    uint numberOfProducts=0;
-    
+
+    mapping(uint => Product) public products;
     mapping(address => uint[]) public sellerToProductIndexes;
-    mapping(bytes32 => uint) public productHashToIndex;
+    //seller to (product to money)
+    mapping(address => mapping(uint => uint)) private owedMoneyToSellers;
+    uint numberOfProducts=0;
+
+    //TODO: createProductInstance(string,uint,link,hashofdata)
 
     function addProduct(string calldata name, uint price, string calldata link, bytes32 marketHashOfData) public {
         require(bytes(name).length != 0,"Name shouldn't be empty");
         require(price>2000000);
-        products.push(Product(name,price,price*100 / 99,msg.sender,block.timestamp,link,marketHashOfData,false));
+        products[numberOfProducts]=(Product(name,price,price*100 / 99,msg.sender,block.timestamp,link,marketHashOfData,false,false,false));
+        numberOfProducts+=1;
     }
 
     function joinMarketplace(address marketplace) public onlyOwner{
@@ -37,4 +43,26 @@ contract SimpleSeller is Ownable{
         require(ownerMarketplace!=address(0),"Doesn't have owner marketplace");
         payable(ownerMarketplace).transfer(address(this).balance);
     }
+
+    function payProduct(uint index) public payable{
+        Product memory p = products[index];
+        require(p.seller!=address(0), "No such product");
+        require(p.paid==false,"Product already bought");
+        require(msg.value>=p.price, "Not enough eth");
+
+        p.paid=true;
+        owedMoneyToSellers[p.seller][index] = p.sellerGets;
+    }
+
+    function deliverProduct(uint index) public /* onlyDelivery */{
+        Product memory p = products[index];
+        require(p.seller!=address(0), "No such product");
+        require(p.paid==true,"Product not paid");
+        require(p.received==false,"Product alredy received");
+        uint pay = owedMoneyToSellers[p.seller][index];
+        owedMoneyToSellers[p.seller][index] = 0;
+        products[index].received=true;
+        payable(p.seller).transfer(pay);
+    }
+
 }
