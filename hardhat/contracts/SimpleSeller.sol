9e99f7d813d36839af063c9d56efffeec93d182d
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-11-20 19:01:10 +0200

    fix: change in addProduct; didn't compile

--- hardhat/contracts/SimpleSeller.sol
+++ hardhat/contracts/SimpleSeller.sol
@@ -14,16 +14,17 @@ contract SimpleSeller is Ownable{
         bytes32 marketHashOfData;
         bool approved;  
     }
-    public address marketplace;
+    address public  marketplace;
     Product[] products;
     uint numberOfProducts=0;
     
-    public mapping(address => uint[]) sellerToProductIndexes;
-    public mapping(bytes32 => uint) productHashToIndex;
+    mapping(address => uint[]) public sellerToProductIndexes;
+    mapping(bytes32 => uint) public productHashToIndex;
 
     function addProduct(string calldata name, uint price, string calldata link, bytes32 marketHashOfData) public {
         require(bytes(name).length != 0);
         require(price>2000000);
-        products.push(new Product(name,price,price*0.99,msg.sender,block.timestamp,link,marketHashOfData,false);
+        products.push(Product(name,price,price*100 / 99,msg.sender,block.timestamp,link,marketHashOfData,false));
     }
+
 }
