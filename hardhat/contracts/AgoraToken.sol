5f3c93e47907075139e04f984efb0f5f698523f5
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-11-27 22:44:00 +0200

    feat: add erc20 contract with transfer functionality via pre-signed transactions

--- hardhat/contracts/AgoraToken.sol
+++ hardhat/contracts/AgoraToken.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract AGRToken is ERC20 {
+    mapping(uint => bool) nonces;
     constructor() ERC20("AgoraToken", "AGR") {
         //_mint(msg.sender, initialSupply);
     }
@@ -13,11 +14,44 @@ contract AGRToken is ERC20 {
         _mint(msg.sender, msg.value);
     }
 
+
     function sellTokens(uint amount) external {
         require(amount>0,"Amount must be >0");
-        require(this.balanceOf(msg.sender)>amount,"You balance is < amount you want to sell");
-
+        require(this.balanceOf(msg.sender)>amount,"Your balance is < amount you want to sell");
         _burn(msg.sender, amount);
         payable(msg.sender).transfer(amount);
+    }
+
+    function splitSignature(bytes[65] calldata sig) internal pure returns (uint8 v , bytes32 r, bytes32 s){
+        require(sig.length == 65);
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function expectedPrefixedMessage(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+    function recoverSigner(bytes32 message, bytes[65] calldata sig) internal pure returns (address){
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+    function sendTokensWithTransaction(uint expiration, uint nonce, uint amount, address from, address to, bytes[65] calldata sig) public {
+        require(expiration>block.timestamp,"Signature expired");
+        require(nonces[nonce]==false,"Signature already used");
+        require(amount>0,"Amount must be >0");
+        require(from!=address(0),"Zero address not allowed");
+        require(to!=address(0),"Zero address not allowed");
+
+        bytes32 message = expectedPrefixedMessage( keccak256(abi.encodePacked(address(this), expiration , nonce, amount, from, to)) );
+        address expectedFrom = recoverSigner(message, sig);
+
+        require(expectedFrom == from, "Signature from wrong user (from!=signature creator)");
+        nonces[nonce]=true;
+
+        _transfer(from, to, amount);
     }
 }
