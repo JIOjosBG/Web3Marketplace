02c79abb51ea2914979a4ee4ee076b761d04ffe6
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-12-21 23:15:24 +0200

    feat: connect to metamask

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,57 @@
 import React, {useState} from 'react';
 import MyNav from './components/Navbar';
 import DisplayProducts from './components/DisplayProducts';
-
+import {ethers} from 'ethers'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [viewingContract, setViewingContract] = useState("");
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [error, setError] = useState("Connect Wallet");
 
   function chnageState(contract){
     setViewingContract(contract);
-    console.log(contract);
   }
 
+  const connectWallet = () => {
+    if(window.ethereum){
+      window.ethereum.request({method: 'eth_requestAccounts'})
+      .then(result => {
+        accountChangedHandler(result[0]);
+      })
+    }else{
+      setError('Install metamask');
+    }
+  }
+
+  const accountChangedHandler = (newAccount) => {
+    console.log(newAccount+"asdasdasd");
+    setAccount(newAccount);
+    getUserBalance(newAccount.toString());
+  }
+
+  const getUserBalance = (address) => {
+    window.ethereum.request({method:'eth_getBalance',params:[address,'latest']})
+    .then(balance => {
+      setBalance(ethers.utils.formatEther(balance));
+    })
+  }
+
+  const chainChnageHandler = () =>{
+    window.location.reload();
+  }
+  window.ethereum.on('accountsChanged',accountChangedHandler);
+  window.ethereum.on('chainChanged',chainChnageHandler);
 
   return (
     <>
+        <h1> {account} </h1>
+      <h1> {balance} </h1>
+      <h1> {error} </h1>
       <MyNav changeState={chnageState} contract={viewingContract}/>
       <DisplayProducts contract={viewingContract}/>
-      
+      <button onClick={connectWallet}>Connect</button>
       <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin="true"></script>
       <script
         src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
