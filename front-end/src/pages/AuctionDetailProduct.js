7221ddb81fda11402183f42e52898cd178c94e39
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-12-26 21:49:39 +0200

    feat: add bidding functionality to simple auction product detail page

--- front-end/src/pages/AuctionDetailProduct.js
+++ front-end/src/pages/AuctionDetailProduct.js
@@ -1,14 +1,21 @@
 import {useState, useEffect} from 'react';
 import {ethers} from 'ethers';
 import { useParams } from 'react-router-dom';
-import {Button} from 'react-bootstrap';
+import {Button, Form} from 'react-bootstrap';
 
 import SimpleAuctionJSON from '../shared/ABIs/SimpleAuction.json';
 import addressesJSON from '../shared/contractAddresses.json';
 
 function AuctionDetailProduct(props){
     const [product,setProduct] = useState(null);
+    const [rate,setRate] = useState(0);
+    const [minimalPriceInUSD,setMinimalPriceInUSD] = useState(0);
+    const [highestBidInUSD,setHighestBidInUSD] = useState(0);
     const simpleAuction= new ethers.Contract( addressesJSON.simpleAuction, SimpleAuctionJSON.abi , props.signer);
+    const [deliveryInstructions,setDeliveryInstructions] = useState("");
+    const [myBid,setMyBid] = useState(0);
+    const [myBidInUSD,setMyBidInUSD] = useState(0);
+    const signer = props.signer;
     const { id } = useParams();
     //TODO: add form for delivery instructions to be passed when purchasing
     //TODO: make popup for that form with amount eth to usd convertion
@@ -22,10 +29,99 @@ function AuctionDetailProduct(props){
             // TODO: check if this is bad ID
             const p = await simpleAuction.products(id);
             setProduct(p);
+            const r = await getRates();
+
+        await setMinimalPriceInUSD(weiToUsd(p.minimalPrice,r));
+        await setHighestBidInUSD(weiToUsd(p.bidAmount,r));
+        
         }catch(e){
             console.log(e);
         }
     }
+
+    const getRates = async () => {
+        try{
+            //https://www.coingecko.com/api/documentations/v3#/simple/get_simple_price
+            const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd', {
+                method: 'GET',
+
+            });
+            let r = (await response.json()).ethereum.usd;
+            setRate(r);
+            return r;
+            
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    const weiToUsd = (wei,r) => {
+        if(wei==0) return 0;
+        let usd = 1/r;
+        const priceInEth = ethers.utils.formatEther(wei);
+        
+        return priceInEth/usd;
+    }
+    const usdToWei = (usd,r) => {
+        let eth = usd/r;
+        eth = parseFloat(eth).toFixed( 18 );
+
+        return ethers.utils.parseEther(eth.toString());
+    }
+
+
+    const handleBidInput = async (amount) => {
+        //TODO: check if it is higher than prev bid and open modal
+        console.log(amount,typeof(amount));
+        if(amount=="") amount="0";
+        amount = parseInt(amount);
+
+        setMyBidInUSD(amount);
+
+        console.log("a");
+        const bidInWei = usdToWei(amount,rate);
+        console.log("aa");
+        console.log(amount,rate,myBidInUSD);
+        setMyBid(bidInWei);
+    }
+
+
+    const makeSignature = async () => {
+        let nonce = await ethers.utils.solidityPack(['address','uint','uint'],[simpleAuction.address,id,myBid]);
+        nonce = await ethers.utils.keccak256(nonce);
+        const signerAddress = await signer.getAddress();
+        let message = await ethers.utils.solidityPack(['uint','bytes32','uint','address','address'],[product.finishDate,nonce,myBid,signerAddress,simpleAuction.address]);
+        message = await ethers.utils.keccak256(message);
+        let signature = signer.signMessage(message);
+        return {"nonce":nonce,"signature":signature};
+    }
+
+    const handleBid = async () => {
+        let nonce,sig;
+        console.log("in");
+        try{
+            const sigData = await makeSignature();
+            nonce=await sigData['nonce'];
+            sig=await sigData['signature'];
+        }catch(e){
+            console.log(e.message);
+        }
+        if(sig==null || deliveryInstructions==""){
+            //TODO: open modal
+            //TODO: encrypt delivery instructions with public key of seller
+            console.log("not okay with sig and delivery instructions");
+        }else{
+            const message = await ethers.utils.solidityPack(['string'],[deliveryInstructions]);
+            const deliveryData = await ethers.utils.arrayify(await ethers.utils.keccak256(message));
+            
+            try{
+                await simpleAuction.bidForProduct(id,deliveryData,product.finishDate,nonce,myBid,await signer.getAddress(),sig);
+            }catch(e){
+                console.log(e);
+            }
+        }
+    }
+
 
     return(
     <>
@@ -33,19 +129,37 @@ function AuctionDetailProduct(props){
         <>
             <h1>{product.name}</h1>
             <img style={{width:'20%'}}src={product.linkForMedia}/>
-            <h2>Minimal price: { parseInt(product.minimalPrice._hex)}</h2>
-            <h2>Highest bid: { parseInt(product.bidAmount._hex)}</h2>
-
+            {product.minimalPrice>product.bidAmount
+            ?<>
+                <h2>Minimal price in wei: { (product.minimalPrice._hex)}</h2>
+                <h6>Minimal in USD: {minimalPriceInUSD}</h6>
+            </>
+            :<>
+                <h2>Highest bid: { (product.bidAmount._hex)}</h2>
+                <h6>Bid in USD: {highestBidInUSD}</h6>
+            </>
+            }
+            <h6>(powered by <a href='https://www.coingecko.com/'> Coingecko </a>)</h6>
+           
             {product.approoved
             ?<h3>Approoved</h3>
             :<></>
             }
             <h4>{product.seller}</h4>
             {new Date(parseInt(product.addDate._hex)*1000).toString()}
-            {/*TODO: make popup form for bid amount*/}
-            {/*TODO: make function for bidding with backend DB*/}
             {/*TODO: make window to shouw previous bids*/}
-            <Button onClick={()=>{console.log("Make bidding function !")}}>Bid now </Button>
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Delivery Instruvtions:</Form.Label>
+                <Form.Control onChange={e=>setDeliveryInstructions(e.target.value)} type="text" placeholder="Delivery instructions"/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Bid for product:</Form.Label>
+                <Form.Control onChange={e=>handleBidInput(e.target.value)} type="number" placeholder="Your bid"/>
+                <Form.Text className="text-muted">{myBid._hex} in Wei</Form.Text>
+
+            </Form.Group>
+            
+            <Button onClick={handleBid}>Bid ${myBidInUSD} </Button>
             </>
         :<h1>Loading</h1>
         }
